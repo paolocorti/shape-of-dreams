c24f0cc391eb45cfb13e66053cc4f865bac81890
Paolo Corti <paolocorti87@gmail.com>
2019-03-07 16:08:05 +0100

    add language and years logic

--- src/Explore1.js
+++ src/Explore1.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import Swipe from 'react-easy-swipe';
+import { groupBy } from 'lodash';
 import Petals from './Petals';
 import topics from './data/topics.json';
 import './Explore.scss';
 
 const Explore1 = ({ history }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedLanguageIndex, setSelectedLanguageIndex] = useState(0);
+  const [selectedYearIndex, setSelectedYearIndex] = useState(0);
 
   const onSwipeRight = () => {
     history.push(`/explore2`);
@@ -19,9 +22,32 @@ const Explore1 = ({ history }) => {
     history.push(`/chapter1`);
   };
 
-  const topic = topics[selectedIndex];
+  const groupedByCountry = groupBy(topics, 'language');
+  const groupedByCountryValues = Object.values(groupedByCountry);
+  const groupedByCountryKeys = Object.keys(groupedByCountry);
+  const years = [
+    2008,
+    2009,
+    2010,
+    2011,
+    2012,
+    2013,
+    2014,
+    2015,
+    2016,
+    2017,
+    2018
+  ];
 
-  console.log(topic);
+  const groupedByYearAndCountry = groupedByCountryValues.map(values => {
+    return Object.values(groupBy(values, 'year'));
+  });
+
+  const languageIndex = groupedByCountryKeys[selectedLanguageIndex];
+  const selectedTopics =
+    groupedByYearAndCountry[selectedLanguageIndex][selectedYearIndex];
+
+  const topic = selectedTopics[selectedIndex];
 
   return (
     <Swipe
@@ -32,7 +58,53 @@ const Explore1 = ({ history }) => {
     >
       <div className='w-100 flex flex-column pa4'>
         <div className='pa4 tc'>Explore1</div>
-        <div className='w-100 flex justify-around pa4'>
+        <div className='w-100 flex justify-around pa2'>
+          <button
+            onClick={() =>
+              setSelectedLanguageIndex(
+                selectedLanguageIndex > 0 ? selectedLanguageIndex - 1 : 0
+              )
+            }
+          >
+            PREV
+          </button>
+          <div>{groupedByCountryKeys[selectedLanguageIndex]} </div>
+          <button
+            onClick={() =>
+              setSelectedLanguageIndex(
+                selectedLanguageIndex < groupedByCountryKeys.length
+                  ? selectedLanguageIndex + 1
+                  : groupedByCountryKeys.length
+              )
+            }
+          >
+            NEXT
+          </button>
+        </div>
+        <div className='w-100 flex justify-around pa2'>
+          <button
+            onClick={() =>
+              setSelectedYearIndex(
+                selectedYearIndex > 0 ? selectedYearIndex - 1 : 0
+              )
+            }
+          >
+            PREV
+          </button>
+          <div>{years[selectedYearIndex]} </div>
+          <button
+            onClick={() =>
+              setSelectedYearIndex(
+                selectedYearIndex < years.length
+                  ? selectedYearIndex + 1
+                  : years.length
+              )
+            }
+          >
+            NEXT
+          </button>
+        </div>
+        <div className='w-100 flex justify-around pa2'>
           <button
             onClick={() =>
               setSelectedIndex(selectedIndex > 0 ? selectedIndex - 1 : 0)
