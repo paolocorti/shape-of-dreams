a5e978053cac9ca781bcd2726b4cdc0059090e06
Paolo Corti <paolocorti87@gmail.com>
2019-03-20 17:11:12 +0100

    minor fix

--- src/Chapter1/Content1.js
+++ src/Chapter1/Content1.js
@@ -3,62 +3,71 @@ import story1 from './cap1-story.png';
 
 const Content1 = ({}) => {
   return (
-    <div className='w-100 flex flex-column pa4'>
+    <div className='w-100 flex flex-column'>
       <h2 className='pa4 tc'>The shapes of our dreams</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In felis dolor,
-        sodales sed dapibus gravida, semper ut nunc. Etiam dignissim purus sed
-        enim malesuada pulvinar. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Nulla id metus magna. Proin tristique mattis lacus,
-        ultrices pretium nunc vehicula rhoncus. Cras iaculis dolor vel quam
-        eleifend iaculis. Curabitur ut arcu eleifend, porta dui nec, molestie
-        libero. Morbi eget sagittis ante. Ut libero turpis, interdum vel enim
-        sit amet, tincidunt interdum orci. Nunc nec nunc eu tortor molestie
-        fermentum vel non orci. Etiam gravida lacus urna, at euismod ante
-        malesuada non. Donec nec porttitor lacus. Phasellus tempor est ornare
-        sem sagittis feugiat. Cras maximus vulputate diam, et lobortis ligula
-        luctus ac. Morbi sagittis orci urna, vel hendrerit magna rutrum
-        fringilla. Etiam porttitor, tortor ut dictum tincidunt, augue felis
-        congue lorem, nec lacinia felis lorem sed orci.
-      </p>
+      <div className='w-100 bg-white pa4'>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In felis
+          dolor, sodales sed dapibus gravida, semper ut nunc. Etiam dignissim
+          purus sed enim malesuada pulvinar. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla id metus magna. Proin tristique
+          mattis lacus, ultrices pretium nunc vehicula rhoncus. Cras iaculis
+          dolor vel quam eleifend iaculis. Curabitur ut arcu eleifend, porta dui
+          nec, molestie libero. Morbi eget sagittis ante. Ut libero turpis,
+          interdum vel enim sit amet, tincidunt interdum orci. Nunc nec nunc eu
+          tortor molestie fermentum vel non orci. Etiam gravida lacus urna, at
+          euismod ante malesuada non. Donec nec porttitor lacus. Phasellus
+          tempor est ornare sem sagittis feugiat. Cras maximus vulputate diam,
+          et lobortis ligula luctus ac. Morbi sagittis orci urna, vel hendrerit
+          magna rutrum fringilla. Etiam porttitor, tortor ut dictum tincidunt,
+          augue felis congue lorem, nec lacinia felis lorem sed orci.
+        </p>
+      </div>
+
       <div className='w-100 tc mt2'>
         <img src={story1} width='90%' alt='chapter 1 story' />
       </div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In felis dolor,
-        sodales sed dapibus gravida, semper ut nunc. Etiam dignissim purus sed
-        enim malesuada pulvinar. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Nulla id metus magna. Proin tristique mattis lacus,
-        ultrices pretium nunc vehicula rhoncus. Cras iaculis dolor vel quam
-        eleifend iaculis. Curabitur ut arcu eleifend, porta dui nec, molestie
-        libero. Morbi eget sagittis ante. Ut libero turpis, interdum vel enim
-        sit amet, tincidunt interdum orci. Nunc nec nunc eu tortor molestie
-        fermentum vel non orci. Etiam gravida lacus urna, at euismod ante
-        malesuada non. Donec nec porttitor lacus. Phasellus tempor est ornare
-        sem sagittis feugiat. Cras maximus vulputate diam, et lobortis ligula
-        luctus ac. Morbi sagittis orci urna, vel hendrerit magna rutrum
-        fringilla. Etiam porttitor, tortor ut dictum tincidunt, augue felis
-        congue lorem, nec lacinia felis lorem sed orci.
-      </p>
+      <div className='w-100 bg-white pa4'>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In felis
+          dolor, sodales sed dapibus gravida, semper ut nunc. Etiam dignissim
+          purus sed enim malesuada pulvinar. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla id metus magna. Proin tristique
+          mattis lacus, ultrices pretium nunc vehicula rhoncus. Cras iaculis
+          dolor vel quam eleifend iaculis. Curabitur ut arcu eleifend, porta dui
+          nec, molestie libero. Morbi eget sagittis ante. Ut libero turpis,
+          interdum vel enim sit amet, tincidunt interdum orci. Nunc nec nunc eu
+          tortor molestie fermentum vel non orci. Etiam gravida lacus urna, at
+          euismod ante malesuada non. Donec nec porttitor lacus. Phasellus
+          tempor est ornare sem sagittis feugiat. Cras maximus vulputate diam,
+          et lobortis ligula luctus ac. Morbi sagittis orci urna, vel hendrerit
+          magna rutrum fringilla. Etiam porttitor, tortor ut dictum tincidunt,
+          augue felis congue lorem, nec lacinia felis lorem sed orci.
+        </p>
+      </div>
+
       <div className='w-100 tc mt2'>
         <img src={story1} width='90%' alt='chapter 1 story' />
       </div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In felis dolor,
-        sodales sed dapibus gravida, semper ut nunc. Etiam dignissim purus sed
-        enim malesuada pulvinar. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Nulla id metus magna. Proin tristique mattis lacus,
-        ultrices pretium nunc vehicula rhoncus. Cras iaculis dolor vel quam
-        eleifend iaculis. Curabitur ut arcu eleifend, porta dui nec, molestie
-        libero. Morbi eget sagittis ante. Ut libero turpis, interdum vel enim
-        sit amet, tincidunt interdum orci. Nunc nec nunc eu tortor molestie
-        fermentum vel non orci. Etiam gravida lacus urna, at euismod ante
-        malesuada non. Donec nec porttitor lacus. Phasellus tempor est ornare
-        sem sagittis feugiat. Cras maximus vulputate diam, et lobortis ligula
-        luctus ac. Morbi sagittis orci urna, vel hendrerit magna rutrum
-        fringilla. Etiam porttitor, tortor ut dictum tincidunt, augue felis
-        congue lorem, nec lacinia felis lorem sed orci.
-      </p>
+      <div className='w-100 bg-white pa4'>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In felis
+          dolor, sodales sed dapibus gravida, semper ut nunc. Etiam dignissim
+          purus sed enim malesuada pulvinar. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla id metus magna. Proin tristique
+          mattis lacus, ultrices pretium nunc vehicula rhoncus. Cras iaculis
+          dolor vel quam eleifend iaculis. Curabitur ut arcu eleifend, porta dui
+          nec, molestie libero. Morbi eget sagittis ante. Ut libero turpis,
+          interdum vel enim sit amet, tincidunt interdum orci. Nunc nec nunc eu
+          tortor molestie fermentum vel non orci. Etiam gravida lacus urna, at
+          euismod ante malesuada non. Donec nec porttitor lacus. Phasellus
+          tempor est ornare sem sagittis feugiat. Cras maximus vulputate diam,
+          et lobortis ligula luctus ac. Morbi sagittis orci urna, vel hendrerit
+          magna rutrum fringilla. Etiam porttitor, tortor ut dictum tincidunt,
+          augue felis congue lorem, nec lacinia felis lorem sed orci.
+        </p>
+      </div>
+
       <div className='w-100 tc mt2'>
         <img src={story1} width='90%' alt='chapter 1 story' />
       </div>
