72c729f7529c447802f39c12485f4b7e79189fe8
Paolo Corti <paolocorti87@gmail.com>
2019-03-24 23:55:50 +0100

    still css fixes

--- src/Chapter1/Explore1.js
+++ src/Chapter1/Explore1.js
@@ -73,7 +73,7 @@ const Explore1 = ({ history }) => {
   return (
     <div className='explore1'>
       <div className='w-100 h-100 pa2 relative'>
-        <div className='w-100'>
+        <div className='w-100' style={{ height: '30% ' }}>
           <LanguageSelector
             languages={groupedByCountryKeys}
             onSelect={onSelectLanguage}
@@ -85,7 +85,7 @@ const Explore1 = ({ history }) => {
             selected={selectedYearIndex}
           />
         </div>
-        <div className='w-100 relative tc'>
+        <div className='w-100 relative tc' style={{ height: '70% ' }}>
           {topic.value ? (
             <Petals
               value={topic.value}
