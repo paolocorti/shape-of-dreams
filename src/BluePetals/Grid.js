ad5ab6af9499f9f643280dc0d2ecab189a29bb27
Paolo Corti <paolocorti87@gmail.com>
2019-04-14 23:50:24 +0200

    fix blue petals grid

--- src/BluePetals/Grid.js
+++ src/BluePetals/Grid.js
@@ -4,212 +4,154 @@ import './BluePetal.scss';
 const Grid = ({}) => {
   return (
     <g>
-      <g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='300' y2='299' />
-            <line className='grid2' x1='300' y1='295.3' x2='300' y2='52.8' />
-            <line className='grid1' x1='300' y1='50.9' x2='300' y2='50' />
-          </g>
-        </g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='300.4' y2='299.2' />
-            <line className='grid2' x1='302.3' y1='296' x2='423.6' y2='85.9' />
-            <line className='grid1' x1='424.5' y1='84.3' x2='425' y2='83.5' />
-          </g>
-        </g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='300.8' y2='299.5' />
-            <line className='grid2' x1='304' y1='297.6' x2='514.1' y2='176.4' />
-            <line className='grid1' x1='515.7' y1='175.5' x2='516.5' y2='175' />
-          </g>
-        </g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='300.9' y2='300' />
-            <line className='grid2' x1='304.6' y1='300' x2='547.2' y2='300' />
-            <line className='grid1' x1='549.1' y1='300' x2='550' y2='300' />
-          </g>
-        </g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='300.8' y2='300.4' />
-            <line className='grid2' x1='304' y1='302.3' x2='514.1' y2='423.6' />
-            <line className='grid1' x1='515.7' y1='424.5' x2='516.5' y2='425' />
-          </g>
-        </g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='300.4' y2='300.8' />
-            <line className='grid2' x1='302.3' y1='304' x2='423.6' y2='514.1' />
-            <line className='grid1' x1='424.5' y1='515.7' x2='425' y2='516.5' />
-          </g>
-        </g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='300' y2='300.9' />
-            <line className='grid2' x1='300' y1='304.6' x2='300' y2='547.2' />
-            <line className='grid1' x1='300' y1='549.1' x2='300' y2='550' />
-          </g>
-        </g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='299.5' y2='300.8' />
-            <line className='grid2' x1='297.6' y1='304' x2='176.4' y2='514.1' />
-            <line className='grid1' x1='175.5' y1='515.7' x2='175' y2='516.5' />
-          </g>
-        </g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='299.2' y2='300.4' />
-            <line className='grid2' x1='296' y1='302.3' x2='85.9' y2='423.6' />
-            <line className='grid1' x1='84.3' y1='424.5' x2='83.5' y2='425' />
-          </g>
-        </g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='299' y2='300' />
-            <line className='grid2' x1='295.3' y1='300' x2='52.8' y2='300' />
-            <line className='grid1' x1='50.9' y1='300' x2='50' y2='300' />
-          </g>
-        </g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='299.2' y2='299.5' />
-            <line className='grid2' x1='296' y1='297.6' x2='85.9' y2='176.4' />
-            <line className='grid1' x1='84.3' y1='175.5' x2='83.5' y2='175' />
-          </g>
-        </g>
-        <g>
-          <g>
-            <line className='grid1' x1='300' y1='300' x2='299.5' y2='299.2' />
-            <line className='grid2' x1='297.6' y1='296' x2='176.4' y2='85.9' />
-            <line className='grid1' x1='175.5' y1='84.3' x2='175' y2='83.5' />
-          </g>
-        </g>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 266.7078 39.3058)'
-          className='grid3 grid4 grid5'
-        >
-          ANIMALS
-        </text>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 271.5303 572.1749)'
-          className='grid3 grid4 grid5'
-        >
-          PEOPLE
-        </text>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 427.822 75.7198)'
-          className='grid3 grid4 grid5'
-        >
-          BODY
-        </text>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 119.0387 75.7198)'
-          className='grid3 grid4 grid5'
-        >
-          OTHER
-        </text>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 525.2802 165.1783)'
-          className='grid3 grid4 grid5'
-        >
-          FLYING
-        </text>
-        <text
-          transform='matrix(1 0 0 1 525.2802 182.7783)'
-          className='grid3 grid4 grid5'
-        >
-          AND
-        </text>
-        <text
-          transform='matrix(1 0 0 1 525.2802 200.3783)'
-          className='grid3 grid4 grid5'
-        >
-          FALLING
-        </text>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 3.9561 165.6822)'
-          className='grid3 grid4 grid5'
-        >
-          WEATHER
-        </text>
-        <text
-          transform='matrix(1 0 0 1 17.4561 183.2822)'
-          className='grid3 grid4 grid5'
-        >
-          EVENTS
-        </text>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 537.3823 271.9807)'
-          className='grid3 grid4 grid5'
-        >
-          FOOD
-        </text>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 16.4586 271.9806)'
-          className='grid3 grid4 grid5'
-        >
-          TRAVELS
-        </text>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 525.2797 444.4586)'
-          className='grid3 grid4 grid5'
-        >
-          MONEY
-        </text>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 21.9586 444.4586)'
-          className='grid3 grid4 grid5'
-        >
-          SUPER-
-        </text>
-        <text
-          transform='matrix(1 0 0 1 8.4586 460.0586)'
-          className='grid3 grid4 grid5'
-        >
-          NATURAL
-        </text>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 428.8304 538.215)'
-          className='grid3 grid4 grid5'
-        >
-          NATURE
-        </text>
-      </g>
-      <g>
-        <text
-          transform='matrix(1 0 0 1 82.2179 538.215)'
-          className='grid3 grid4 grid5'
-        >
-          PREGNANCY
-        </text>
-      </g>
+<g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="300" y2="299"/>
+			<line className="grid2" x1="300" y1="295.3" x2="300" y2="52.8"/>
+			<line className="grid1" x1="300" y1="50.9" x2="300" y2="50"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="300.4" y2="299.2"/>
+			<line className="grid2" x1="302.3" y1="296" x2="423.6" y2="85.9"/>
+			<line className="grid1" x1="424.5" y1="84.3" x2="425" y2="83.5"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="300.8" y2="299.5"/>
+			<line className="grid2" x1="304" y1="297.6" x2="514.1" y2="176.4"/>
+			<line className="grid1" x1="515.7" y1="175.5" x2="516.5" y2="175"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="300.9" y2="300"/>
+			<line className="grid2" x1="304.6" y1="300" x2="547.2" y2="300"/>
+			<line className="grid1" x1="549.1" y1="300" x2="550" y2="300"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="300.8" y2="300.4"/>
+			<line className="grid2" x1="304" y1="302.3" x2="514.1" y2="423.6"/>
+			<line className="grid1" x1="515.7" y1="424.5" x2="516.5" y2="425"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="300.4" y2="300.8"/>
+			<line className="grid2" x1="302.3" y1="304" x2="423.6" y2="514.1"/>
+			<line className="grid1" x1="424.5" y1="515.7" x2="425" y2="516.5"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="300" y2="300.9"/>
+			<line className="grid2" x1="300" y1="304.6" x2="300" y2="547.2"/>
+			<line className="grid1" x1="300" y1="549.1" x2="300" y2="550"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="299.5" y2="300.8"/>
+			<line className="grid2" x1="297.6" y1="304" x2="176.4" y2="514.1"/>
+			<line className="grid1" x1="175.5" y1="515.7" x2="175" y2="516.5"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="299.2" y2="300.4"/>
+			<line className="grid2" x1="296" y1="302.3" x2="85.9" y2="423.6"/>
+			<line className="grid1" x1="84.3" y1="424.5" x2="83.5" y2="425"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="299" y2="300"/>
+			<line className="grid2" x1="295.3" y1="300" x2="52.8" y2="300"/>
+			<line className="grid1" x1="50.9" y1="300" x2="50" y2="300"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="299.2" y2="299.5"/>
+			<line className="grid2" x1="296" y1="297.6" x2="85.9" y2="176.4"/>
+			<line className="grid1" x1="84.3" y1="175.5" x2="83.5" y2="175"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<line className="grid1" x1="300" y1="300" x2="299.5" y2="299.2"/>
+			<line className="grid2" x1="297.6" y1="296" x2="176.4" y2="85.9"/>
+			<line className="grid1" x1="175.5" y1="84.3" x2="175" y2="83.5"/>
+		</g>
+	</g>
+</g>
+<g>
+	<g>
+		<text transform="matrix(1 0 0 1 254.448 582.1749)" className="grid3 grid4 grid5">PEOPLE</text>
+	</g>
+</g>
+<g>
+	<g>
+		<text transform="matrix(1 0 0 1 91.7966 75.7198)" className="grid3 grid4 grid5">OTHER</text>
+	</g>
+</g>
+<g>
+	<text transform="matrix(1 0 0 1 3.1158 128.427)" className="grid3 grid4 grid5">WEATHER</text>
+	<text transform="matrix(1 0 0 1 15.7761 156.7706)" className="grid3 grid4 grid5">EVENTS</text>
+</g>
+<g>
+	<g>
+		<text transform="matrix(1 0 0 1 531.5929 342.7397)" className="grid3 grid4 grid5">FOOD</text>
+	</g>
+</g>
+<g>
+	<g>
+		<text transform="matrix(1 0 0 1 3.9562 342.7399)" className="grid3 grid4 grid5">TRAVELS</text>
+	</g>
+</g>
+<g>
+	<g>
+		<text transform="matrix(1 0 0 1 510.2797 463.4586)" className="grid3 grid4 grid5">MONEY</text>
+	</g>
+</g>
+<g>
+	<g>
+		<text transform="matrix(1 0 0 1 15.3013 463.4593)" className="grid3 grid4 grid5">SUPER-</text>
+		<text transform="matrix(1 0 0 1 2.3062 489.0593)" className="grid3 grid4 grid5">NATURAL</text>
+	</g>
+</g>
+<g>
+	<g>
+		<text transform="matrix(1 0 0 1 428.8304 550.215)" className="grid3 grid4 grid5">NATURE</text>
+	</g>
+</g>
+<g>
+	<g>
+		<text transform="matrix(1 0 0 1 26.2838 549.5499)" className="grid3 grid4 grid5">PREGNANCY</text>
+	</g>
+</g>
+<g>
+	<g>
+		<text transform="matrix(1 0 0 1 246.4824 39.3063)" className="grid3 grid4 grid5">ANIMALS</text>
+	</g>
+</g>
+<g>
+	<g>
+		<text transform="matrix(1 0 0 1 427.9028 75.7192)" className="grid3 grid4 grid5">BODY</text>
+	</g>
+</g>
+<g>
+	<g>
+		<text transform="matrix(1 0 0 1 504.4673 100.083)" className="grid3 grid4 grid5">FLYING</text>
+		<text transform="matrix(1 0 0 1 521.2183 128.4268)" className="grid3 grid4 grid5">AND</text>
+		<text transform="matrix(1 0 0 1 497.1714 156.7705)" className="grid3 grid4 grid5">FALLING</text>
+	</g>
+</g>
     </g>
   );
 };
