42e90795d283a52b46f15151ef9ae311eb25b079
Paolo Corti <paolocorti87@gmail.com>
2019-06-01 13:55:52 +0200

    padding fix on mobile

--- src/Chapter1/Content1.js
+++ src/Chapter1/Content1.js
@@ -36,13 +36,13 @@ const Content1 = ({ history, activeIndex }) => {
       }
       <h4 className='tl fw6 mv0 ph4'>Chapter 1</h4>
       <h1 className='tl fw7 mt0 ph4'>The Shapes of our Dreams</h1>
-      <div className='tj f4 ph5 mt5'>
+      <div className='tj f4 ph4 ph5-ns mt5'>
         <p className='lh-copy'>How do we search for the meaning of our dreams on Google? We have explored the related queries to the question <span className='highlights'>What does it mean to dream about</span> in different languages to see what are the dreams’ subjects that the users look for the most. We discovered that we share some recurring dreams.</p>
       </div>
       <div className='w-100 tc mt4'>
         <img src={isMobile ? mobileContent1 : content1} width='100%' alt='chapter 1' />
       </div>
-      <div className='tj f4 ph5 mt5'>
+      <div className='tj f4 ph4 ph5-ns mt5'>
         <p className='lh-copy'>
           We studied the results extracted translating questions such as <span className='highlights'>What does it mean to dream about..., Why do I dream about..., Meaning of dreaming...</span> in seven languages and we’ve explored the topic over the years, from 2008 to 2018.
           We started from the top spoken languages and we selected the ones for which we have found enough data to explore the topic.
@@ -51,7 +51,7 @@ const Content1 = ({ history, activeIndex }) => {
       <div className='w-100 tc mt5'>
         <img src={isMobile ? mobileContent1a : content1a} width='100%' alt='chapter 1' />
       </div>
-      <div className='tj f4 ph5 mt5'>
+      <div className='tj f4 ph4 ph5-ns mt5'>
         <p className='lh-copy'>
           There are some subjects whose meaning people consistently look for: dreaming about <span className='highlights'>snakes</span> and <span className='highlights'>falling teeth</span> for instance: they appeared in all the explored languages from 2008 to 2018.
         </p>
@@ -70,7 +70,7 @@ const Content1 = ({ history, activeIndex }) => {
       <div className='w-100 tc mt4'>
         <img src={isMobile ? mobileContent3 : content3} width='100%' alt='chapter 1' />
       </div>
-      <div className='tj f4 ph5 mt5'>
+      <div className='tj f4 ph4 ph5-ns mt5'>
         <p className='lh-copy'>
           On the other hand, there are unique subjects that only appeared exploring specific languages: dreaming about <span className='highlights'>cabbage</span> only appeared in Russian, dreaming about <span className='highlights'>camels</span> in Arabic, dreaming about <span className='highlights'>garlic</span> in Portuguese, about <span className='highlights'>iguanas</span> in Spanish, and about <span className='highlights'>fox</span> in English.
         </p>
@@ -90,7 +90,7 @@ const Content1 = ({ history, activeIndex }) => {
         <img src={isMobile ? mobileContent4 : content4} width='100%' alt='chapter 1' />
       </div>
 
-      <div className='tj f4 ph5 mt5'>
+      <div className='tj f4 ph4 ph5-ns mt5'>
         <p className='lh-copy'>
           And then we’ve found interesting data exploring subjects with peaks in the search interest over the years. In 2011 the related query with the highest value in Japanese was <span className='highlights'>earthquake</span>, in 2016 the one with the highest value in Arabic was <span className='highlights'>traveling</span>.
         </p>
@@ -106,7 +106,7 @@ const Content1 = ({ history, activeIndex }) => {
       <div className='w-100 tc mt4'>
         <img src={isMobile ? mobileContent5 : content5} width='100%' alt='chapter 1' />
       </div>
-      <div className='tj f4 ph5 mt5'>
+      <div className='tj f4 ph4 ph5-ns mt5'>
         <p className='lh-copy'>
           Are you curious to explore the dreams by year and language? We’ve designed a dream explorer!        </p>
       </div>
--- src/Chapter2/Content2.js
+++ src/Chapter2/Content2.js
@@ -15,7 +15,7 @@ const Content2 = ({ history }) => {
     <div className='w-100 bg-white read'>
       <h4 className='tl fw6 mv0 ph4'>Chapter 2</h4>
       <h1 className='tl fw7 mt0 ph4'>The Worlds We Dream of</h1>
-      <div className='tj f4 ph5 mt5'>
+      <div className='tj f4 ph4 ph5-ns mt5'>
         <p className='lh-copy'>
           What are the worlds we dream of the most according to Google searches? After exploring the subjects, we created some macro-categories of dreams to see what are the universes that populate our sleep and how they change according to the different languages over the years.
         </p>
@@ -23,7 +23,7 @@ const Content2 = ({ history }) => {
       <div className='w-100 tc mt5'>
         <img src={isMobile ? mobileContent1 : content1} width='100%' alt='chapter 2' />
       </div>
-      <div className='tj f4 ph5 mt5'>
+      <div className='tj f4 ph4 ph5-ns mt5'>
         <p className='lh-copy'>
           After having studied all the dreams’ subjects by language and year, we grouped them into 12 categories: <span className='highlights'>Animals and Insects</span> (where we grouped subjects such as snakes, spiders, dogs), <span className='highlights'>Human Body</span> (with subjects such as falling teeth, hair, nails), <span className='highlights'>Family and relationships</span> (with subjects such as having a baby, getting married, brother, ex partner), <span className='highlights'>Falling and Flying</span> (with subjects such as flying and falling into the void), <span className='highlights'>Emotions</span> (with subjects such as crying, laughing, scary dreams), <span className='highlights'>Food</span> (with subjects such as rice, strawberry, bread), <span className='highlights'>Natural Elements</span> (with subjects such as fire, water, sand), <span className='highlights'>Weather events</span> (with subjects such as storm, tornado, rain), <span className='highlights'>Traveling and transportation</span> (with subjects such as train, travels, airplane), <span className='highlights'>Money</span> (with subjects such as winning money, diamonds, jewels), <span className='highlights'>Supernatural</span> (with subjects such as zombies, werewolves, elves) and <span className='highlights'>Other</span>.
         </p>
@@ -34,14 +34,14 @@ const Content2 = ({ history }) => {
       <div className='w-100 tc mt5'>
         <img src={isMobile ? mobileContent2 : content2} width='100%' alt='chapter 2' />
       </div>
-      <div className='tj f4 ph5 mt5'>
+      <div className='tj f4 ph4 ph5-ns mt5'>
         <p className='lh-copy'>
           Also exploring the less prominent categories is interesting: we discovered that over the years the language with more <span className='highlights'>Emotions</span> related subjects is Japanese and we noticed a <span className='highlights'>Food</span> peak in Russian in 2012.        </p>
       </div>
       <div className='w-100 tc mt5'>
         <img src={isMobile ? mobileContent3 : content3} width='100%' alt='chapter 2' />
       </div>
-      <div className='tj f4 ph5 mt5'>
+      <div className='tj f4 ph4 ph5-ns mt5'>
         <p className='lh-copy'>
           Time to further explore the categories! Go to our dreams explorer!
         </p>
