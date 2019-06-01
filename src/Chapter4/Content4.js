90e8cd5c32a734c43ca423146d535bfb4b171af7
Paolo Corti <paolocorti87@gmail.com>
2019-06-01 14:24:26 +0200

    add read 4

--- src/Chapter4/Content4.js
+++ src/Chapter4/Content4.js
@@ -1,5 +1,17 @@
-import React, {useContext} from 'react';
-import content1 from './cap4-1.jpg'
+import React, { useContext } from 'react';
+import content1 from './cap4-01-desk.jpg'
+import content2 from './cap4-02-desk.jpg'
+import content3 from './cap4-03-desk.jpg'
+import content4 from './cap4-04-desk.jpg'
+import mobileContent1 from './cap4-01-mob.jpg'
+import mobileContent2 from './cap4-02-mob.jpg'
+import mobileContent3 from './cap4-03-mob.jpg'
+import mobileContent4 from './cap4-04-mob.jpg'
+import dna from './dna.svg'
+import dog from './dog.svg'
+import fish from './fish.svg'
+import toad from './toad.svg'
+import elephant from './elephant.svg'
 import { AppContext } from '../appContext';
 import { isMobile } from 'react-device-detect';
 
@@ -9,33 +21,71 @@ const Content4 = ({ history }) => {
   return (
     <div className='w-100 bg-white read'>
       <h4 className='tl fw6 mv0 ph4'>Chapter 4</h4>
-      <h1 className='tl fw7 mt0 ph4'>Dreams that Connect Us</h1>
-      <div className='ph4 tj f4'>
-      <p className='lh-title'>
-      What are the dreams that have been looked for in more than one language? We have analyzed the network of dreams to see what are the subjects that create such connections.
+      <h1 className='tl fw7 mt0 ph4'>Dreams that connect us</h1>
+      <div className='tj f4 ph4 ph5-ns mt5'>
+        <p className='lh-copy'>
+          Another aspect we were interested in was exploring the networks of dreams: how do the dreams’s subjects – whose meaning people look for on Google – connect different languages? For this reason we visualized the dreams that connect us by year.
         </p>
       </div>
-      <div className='w-100 tc mv4'>
-        <img src={content1} width='100%' alt='chapter 4' />
+      <div className='w-100 tc mt5'>
+        <img src={isMobile ? mobileContent1 : content1} width='100%' alt='chapter 2' />
       </div>
-      <div className='ph4 tj f4'>
-        <p className='lh-title'>
-        Tap on START EXPLORING to see the networks of dreams.
+      <div className='tj f4 ph4 ph5-ns mt5'>
+        <p className='lh-copy'>
+          The dreams’s subjects that connect more languages are the ones that more consistently appeared in our explorations: snakes and falling teeth. But there are also other subjects that often connected the languages, such as <span className='highlights'>being pregnant, dogs</span> and <span className='highlights'>fishes.</span>
         </p>
       </div>
-      <div className='w-100 tc pb4'>
-        <div className='flex flex-column ph4'>
-          <div className='raleway fw7 tl' style={{ fontSize: '16px', color: '#43449a', letterSpacing: '1px'}}>START EXPLORING</div>
-          <img src={'images/down-arrow.svg'} width={35} onClick={() => context.setSelectedView('explore')} />
+      <div className='w-100 tc mt5 flex flex-column items-center'>
+        <div className='w-100 flex justify-center items-center'>
+          <img src={dna} height={120} alt='dna' />
+          <img src={dog} height={140} alt='dog' />
+          <img src={fish} height={110} alt='fish' />
         </div>
-        {
-          !isMobile && (
-            <div className='flex flex-column ph4'>
-              <div className='raleway fw7 tl' style={{ fontSize: '16px', color: '#43449a', letterSpacing: '1px'}}>BACK TO CHAPTER 3</div>
-              <img src={'images/down-arrow.svg'} width={35} onClick={() => history.push(`/chapter3`)} />
+        {/* <h2 className='mv3'>Peaks over the year</h2> */}
+      </div>
+      <div className='w-100 tc mt5'>
+        <img src={isMobile ? mobileContent2 : content2} width='100%' alt='chapter 2' />
+      </div>
+      <div className='tj f4 ph4 ph5-ns mt5'>
+        <p className='lh-copy'>
+          There are subjects that connected a specific language with the other ones in more than one year: such as <span className='highlights'>crying</span>, that linked Arabic with English in 2013 and 2018 and with Spanish and French in 2014.
+        </p>
+      </div>
+      <div className='w-100 tc mt5'>
+        <img src={isMobile ? mobileContent3 : content3} width='100%' alt='chapter 2' />
+      </div>
+      <div className='tj f4 ph4 ph5-ns mt5'>
+        <p className='lh-copy'>
+          We also found unique connections between couple of languages: such as dreaming about <span className='highlights'>elephants</span>, that connected English and Portuguese in 2011 and 2012 and dreaming about <span className='highlights'>toads</span>, that linked Portuguese and Spanish in 2015 and 2017.        </p>
+      </div>
+      <div className='w-100 tc mt5 flex flex-column items-center'>
+        <div className='w-100 flex justify-center items-center'>
+          <img src={toad} height={110} alt='toad' />
+          <img src={elephant} height={140} alt='elephant' />
+        </div>
+        {/* <h2 className='mv3'>Peaks over the year</h2> */}
+      </div>
+      <div className='w-100 tc mt5'>
+        <img src={isMobile ? mobileContent4 : content4} width='100%' alt='chapter 2' />
+      </div>
+      <div className='tj f4 ph4 ph5-ns mt5'>
+        <p className='lh-copy'>
+          Are you curious to explore the networks? Go to our dreams explorer!
+        </p>
+      </div>
+      <div className='w-100 tc pb4 flex items-center flex-column'>
+        <div className='flex flex-column items-center ph4 pv3'>
+          <div className='raleway fw7 tl' style={{ fontSize: '22px', color: '#43449a', letterSpacing: '1px' }}>START EXPLORING</div>
+          <img className='mt2' src={'images/down-arrow.svg'} width={35} onClick={() => context.setSelectedView('explore')} />
+        </div>
+        {/* {
+          isMobile && (
+            <div className='flex flex-column items-center ph4 pv3'>
+              <div className='raleway fw7 tl' style={{ fontSize: '22px', color: '#43449a', letterSpacing: '1px' }}>READ CHAPTER 2</div>
+              <img className='mt2' src={'images/down-arrow.svg'} width={35} onClick={() => history.push(`/chapter2`)} />
             </div>
           )
-        }
+        } */}
       </div>
     </div>
   );
