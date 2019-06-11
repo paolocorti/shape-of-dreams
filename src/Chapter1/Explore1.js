b3ca80f9a40ebb72978566dea7ebac60da55f307
Paolo Corti <paolocorti87@gmail.com>
2019-06-11 10:34:35 +0200

    fix the icons

--- src/Chapter1/Explore1.js
+++ src/Chapter1/Explore1.js
@@ -213,13 +213,13 @@ const Explore1 = ({ history, activeIndex }) => {
                 {selectedTopics.length > 0 && selectedIndex > 0 && (
                   <div className='left-tap pointer' onClick={() => onTapLeft()}>
                     <div className='icon-circle'></div>
-                    <img className='icon-circle-arrow' src={'/images/arrow-left.svg'} alt='Tap icon' width={12} />
+                    <img className='icon-circle-arrow' src={'/images/arrow-left.svg'} alt='Tap icon' width={10} />
                   </div>
                 )}
                 {selectedTopics.length > 0 && (
                   <div className='right-tap pointer' onClick={() => onTapRight()}>
                     <div className='icon-circle'></div>
-                    <img className='icon-circle-arrow' src={'/images/arrow-right.svg'} alt='Tap icon' width={12} />
+                    <img className='icon-circle-arrow' src={'/images/arrow-right.svg'} alt='Tap icon' width={10} />
                   </div>
                 )}
               </div>
