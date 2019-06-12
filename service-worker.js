7a6a6b6c969acf623ce7b53edfd4afbf5194cf16
Paolo Corti <paolocorti87@gmail.com>
2019-06-12 17:59:07 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f317e105bdf7eaaeefdb780c61be78b.js"
+  "/precache-manifest.018a8b717f6284696f295327786b58ee.js"
 );
 
 workbox.clientsClaim();
