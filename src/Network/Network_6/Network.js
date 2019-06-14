4b39115a76d83616f96d9510a18263f16c30c0b1
Paolo Corti <paolocorti87@gmail.com>
2019-06-14 10:45:18 +0200

    fix again netowrk dimension

--- src/Network/Network_6/Network.js
+++ src/Network/Network_6/Network.js
@@ -38,7 +38,7 @@ const Network6 = ({ year }) => {
         viewBox='0 0 430 430'
         width={svgWidth}
         height={svgHeight}
-        style={{ maxWidth: isMobile ? '420px' : '520px' }}
+        style={{ maxWidth: isMobile ? '400px' : '520px' }}
       >
         <NetworkLines selected={selected} />
         <NetworkCircles selected={selected} setSelected={selectedCallback} />
