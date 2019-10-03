59e2b08062f2ad841410d7038f085db2b1ece517
Paolo Corti <paolocorti87@gmail.com>
2019-10-03 20:48:36 +0200

    add reload on screen orientation change

--- src/Routes.js
+++ src/Routes.js
@@ -14,6 +14,7 @@ import Menu from './components/Menu';
 import Legend from './components/Legend';
 import About from './components/About';
 import { isMobileWithTablet } from './constants';
+import DeviceOrientation from 'react-screen-orientation'
 
 
 const Routes = ({ location }) => {
@@ -21,39 +22,41 @@ const Routes = ({ location }) => {
   const context = useContext(AppContext);
 
   return (
-    <div className={`w-100 ${pathname === '/' ? 'h-100' : ''}`} style={{ height: isMobileWithTablet ? 'auto' : '100%', overflow: isMobileWithTablet ? 'auto' : 'hidden' }}>
-      {pathname !== '/' && <Header />}
-      {pathname !== '/' && <Menu />}
-      {pathname !== '/' && <Legend />}
-      {pathname !== '/' && <About />}
-      <Route exact path='/' component={Landing} />
-      {pathname !== '/' && isMobileWithTablet && (
-        <SwipeableRoutes
-          containerStyle={{
-            height:
-              context.selectedView === 'read' ? 'calc(100vh - 50px)' : '100%'
-          }}
-          onChangeIndex={context.resetSelectedView}
-        >
-          <Route exact path={'/chapter1'} component={Chapter1} />
-          <Route exact path={'/chapter2'} component={Chapter2} />
-          <Route exact path={'/chapter3'} component={Chapter3} />
-          <Route exact path={'/chapter4'} component={Chapter4} />
-        </SwipeableRoutes>
-      )}
-      {
-        !isMobileWithTablet && (
-          <Switch>
-            <Route exact path={'/chapter1'} component={Container} />
-            <Route exact path={'/chapter2'} component={Container} />
-            <Route exact path={'/chapter3'} component={Container} />
-            <Route exact path={'/chapter4'} component={Container} />
-          </Switch>
-        )
-      }
+    <DeviceOrientation onOrientationChange={() => window.location.reload()}>
+      <div className={`w-100 ${pathname === '/' ? 'h-100' : ''}`} style={{ height: isMobileWithTablet ? 'auto' : '100%', overflow: isMobileWithTablet ? 'auto' : 'hidden' }}>
+        {pathname !== '/' && <Header />}
+        {pathname !== '/' && <Menu />}
+        {pathname !== '/' && <Legend />}
+        {pathname !== '/' && <About />}
+        <Route exact path='/' component={Landing} />
+        {pathname !== '/' && isMobileWithTablet && (
+          <SwipeableRoutes
+            containerStyle={{
+              height:
+                context.selectedView === 'read' ? 'calc(100vh - 50px)' : '100%'
+            }}
+            onChangeIndex={context.resetSelectedView}
+          >
+            <Route exact path={'/chapter1'} component={Chapter1} />
+            <Route exact path={'/chapter2'} component={Chapter2} />
+            <Route exact path={'/chapter3'} component={Chapter3} />
+            <Route exact path={'/chapter4'} component={Chapter4} />
+          </SwipeableRoutes>
+        )}
+        {
+          !isMobileWithTablet && (
+            <Switch>
+              <Route exact path={'/chapter1'} component={Container} />
+              <Route exact path={'/chapter2'} component={Container} />
+              <Route exact path={'/chapter3'} component={Container} />
+              <Route exact path={'/chapter4'} component={Container} />
+            </Switch>
+          )
+        }
 
-      {pathname !== '/' && <NavigationFooter />}
-    </div>
+        {pathname !== '/' && <NavigationFooter />}
+      </div>
+    </DeviceOrientation>
   );
 };
 
